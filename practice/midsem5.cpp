ef4fb4b8ee7286eb6450143fb3bea21f6e7f4502
abhinavsinhak <shuhantgaurav2005@gmail.com>
2023-03-15 08:17:19 +0530

    new

--- practice/midsem5.cpp
+++ practice/midsem5.cpp
@@ -1,54 +1,62 @@
 #include <iostream>
 using namespace std;
 
-class Person {
+class Person
+{
 protected:
     std::string name;
     int age;
+
 public:
     Person(std::string name, int age) : name(name), age(age) {}
     virtual int getBonus() const { return 0; }
     virtual ~Person() {}
 };
 
-class Admin : virtual public Person {
+class Admin : virtual public Person
+{
 protected:
     int salary;
+
 public:
     Admin(std::string name, int age, int salary) : Person(name, age), salary(salary) {}
     virtual int getBonus() const override { return salary * 0.1; }
 };
 
-class Account : virtual public Person {
+class Account : virtual public Person
+{
 protected:
     int balance;
+
 public:
     Account(std::string name, int age, int balance) : Person(name, age), balance(balance) {}
     virtual int getBonus() const override { return balance * 0.05; }
 };
 
-class Master : public Admin, public Account {
+class Master : public Admin, public Account
+{
 public:
     Master(std::string name, int age, int salary, int balance)
-        : Person(name, age), Admin(name, age, salary), Account(name, age, balance) {};
+        : Person(name, age), Admin(name, age, salary), Account(name, age, balance){};
 
-    virtual int getBonus() const override { return Admin::getBonus() + Account::getBonus();}
+    virtual int getBonus() const override { return Admin::getBonus() + Account::getBonus(); }
 };
 
-
-int main() {
+int main()
+{
     const int NUM_EMPLOYEES = 3;
-    Person* employees[NUM_EMPLOYEES] = {
+    Person *employees[NUM_EMPLOYEES] = {
         new Admin("John", 30, 50000),
         new Account("Mary", 25, 100000),
-        new Master("Bob", 40, 70000, 200000)
-    };
+        new Master("Bob", 40, 70000, 200000)};
 
-    for (int i = 0; i < NUM_EMPLOYEES; i++) {
+    for (int i = 0; i < NUM_EMPLOYEES; i++)
+    {
         cout << employees[i]->getBonus() << endl;
     }
 
-    for (int i = 0; i < NUM_EMPLOYEES; i++) {
+    for (int i = 0; i < NUM_EMPLOYEES; i++)
+    {
         delete employees[i];
     }
 
