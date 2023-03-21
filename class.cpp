a1bbdb72e64f49463582121b9b5bb07ffb636698
abhinavsinhak <shuhantgaurav2005@gmail.com>
2023-03-22 02:53:29 +0530

    new

--- class.cpp
+++ class.cpp
@@ -1,18 +1,79 @@
 #include <iostream>
 using namespace std;
-class student
-{  public:
-    int age;
-    char name[20];
+
+template <class T>
+class Stack
+{
+private:
+    T *arr;
+    int capacity;
+    int top;
+
+public:
+    Stack(int size)
+    {
+        arr = new T[size];
+        capacity = size;
+        top = -1;
+    }
+    ~Stack()
+    {
+        delete[] arr;
+    }
+    void push(T x)
+    {
+        if (isFull())
+        {
+            cout << "Stack overflow\n";
+            exit(EXIT_FAILURE);
+        }
+        cout << "Inserting " << x << "\n";
+        arr[++top] = x;
+    }
+    T pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack underflow\n";
+            exit(EXIT_FAILURE);
+        }
+        cout << "Removing " << peek() << "\n";
+        return arr[top--];
+    }
+    T peek()
+    {
+        if (!isEmpty())
+        {
+            return arr[top];
+        }
+        else
+        {
+            exit(EXIT_FAILURE);
+        }
+    }
+    bool isEmpty()
+    {
+        return top == -1;
+    }
+
+    bool isFull()
+    {
+        return top == capacity - 1;
+    }
 };
 
-int main(void)
-{    
-    student s1;
-        cin>>s1.name;
-        cin>>s1.age;
+int main()
+{
+    Stack<int> integer_stack(3);
 
-        cout<<s1.name<<endl<<s1.age<<endl;
-        
-        
+    integer_stack.push(1);
+    integer_stack.push(2);
+
+    integer_stack.pop();
+
+    integer_stack.push(3);
+
+    cout << "The top element is " << integer_stack.peek() << endl;
+
+    return 0;
 }
--- class2.cpp
+++ class2.cpp
@@ -1,46 +1,79 @@
 #include <iostream>
 using namespace std;
-class hero
+
+template <class T>
+class Stack
 {
-    int health;
-    char level;
+private:
+    T *arr;
+    int capacity;
+    int top;
 
 public:
-    int gethealth()
+    Stack(int size)
     {
-        return health;
+        arr = new T[size];
+        capacity = size;
+        top = -1;
     }
-    void sethealth(int n)
+    ~Stack()
     {
-        health = n;
+        delete[] arr;
     }
-    char getlevel(){
+    void push(T x)
+    {
+        if (isFull())
+        {
+            cout << "Stack overflow\n";
+            exit(EXIT_FAILURE);
+        }
+        cout << "Inserting " << x << "\n";
+        arr[++top] = x;
+    }
+    T pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack underflow\n";
+            exit(EXIT_FAILURE);
+        }
+        cout << "Removing " << peek() << "\n";
+        return arr[top--];
+    }
+    T peek()
+    {
+        if (!isEmpty())
+        {
+            return arr[top];
+        }
+        else
+        {
+            exit(EXIT_FAILURE);
+        }
+    }
+    bool isEmpty()
+    {
+        return top == -1;
+    }
 
-        return level;
-    }
-    void setlevel(char t)
+    bool isFull()
     {
-        level = t;
+        return top == capacity - 1;
     }
-    
 };
 
 int main()
 {
+    Stack<int> integer_stack(3);
 
-    hero h1;
-    int n1;char n2;
-    cout << "give health" << endl;
-    cin>>n1;
-    h1.sethealth(n1);
-    cout << "give the level" << endl;
-    cin>>n2;
-    h1.setlevel(n2);
+    integer_stack.push(1);
+    integer_stack.push(2);
 
-    // printing the level hh
-     
-    cout<<"health\n"<<h1.gethealth()<<endl;
-    cout<<"level\n"<<h1.getlevel()<<endl;
+    integer_stack.pop();
+
+    integer_stack.push(3);
+
+    cout << "The top element is " << integer_stack.peek() << endl;
 
     return 0;
 }
